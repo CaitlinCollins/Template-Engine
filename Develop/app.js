079bb66b8e1b735aba482ec5a70b2b3a97fd60b9
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-02-19 19:24:28 -0600

    Added question arrays for the various roles.

--- Develop/app.js
+++ Develop/app.js
@@ -1,14 +1,109 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
+// const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+const managerQuestions = [
+    {
+        name: "welcome",
+        message: "Welcome to your Team Roster Builder! Let's get started!",
+    },
+    { 
+        type: "input",
+        name: "managerName",
+        message: "Let's start with the manager. What is their name?",
+
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's employee id?",
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?",
+    },
+    {
+        type: "input",
+        name: "managerOffice",
+        message: "What is the manager's office number?",
+    },
+    {
+        type: "confirm",
+        name: "next",
+        message: "The manager has been added to the roster. Would you like to add another employee?",
+    },
+];
+
+const engineerQuestions = [
+    { 
+        type: "input",
+        name: "engineerName",
+        message: "Great! Let's add an engineer to the roster! What is their name?",
+
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's employee id?",
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?",
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's GitHub username?",
+
+    },
+    {
+        type: "confirm",
+        name: "next2",
+        message: "The engineer has been added to the roster. Would you like to add another employee?",
+    },
+
+],
+
+const internQuestions = [
+    { 
+        type: "input",
+        name: "internName",
+        message: "Great! Let's add an intern to the roster! What is their name?",
+
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's employee id?",
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?",
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "What school does the intern attend?",
+
+    },
+    {
+        type: "confirm",
+        name: "next2",
+        message: "The engineer has been added to the roster. Would you like to add another employee?",
+    },
+
+],
 
 
 // Write code to use inquirer to gather information about the development team members,
